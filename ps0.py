c70fc02afd3d20fd09899357bc042ac4937db5ba
Ricardo Ponce <85263893+leorick139@users.noreply.github.com>
2021-07-09 15:11:48 +0000

    completed assignment

--- ps0.py
+++ ps0.py
@@ -1,10 +1,12 @@
+import math
 # 1. Ask the user to enter a number "x"
-
+x = int(input('Enter number x:'))
 
 # 2. Ask the user to enter a number "y"
-
+y = int(input('Enter number y:'))
 
 # 3. Prints out the number "x" raised to the power "y"
-
+print('x^y =',x**y)
 
 # 4. Prints out the log (base 2) of "x"
+print('log(x) = ',math.log(x,2))
